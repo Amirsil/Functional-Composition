ee672fc5e5dd826416c61d3240f6ee763f38b5b7
Amirsil <57900754+Amirsil@users.noreply.github.com>
2021-08-27 00:46:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from timer import measure_time
-from functional_utils import filter_divisable_by, infinity_from, power_by, until, get_n_items
+from functional_utils import divisable_by, infinity_from, power_by, until, get_n_items
 from concat import APPLY
 if __name__ == "__main__":
     APPLY << print << '\n'.join << list << 'HELLO'
